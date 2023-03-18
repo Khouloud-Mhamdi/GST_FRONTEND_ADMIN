56f238d6260a5c3c2b6775644674aa7fb9fe84a4
khouloud <khouloudmhamdi7@gmail.com>
2023-03-18 01:49:34 +0100

    pagination

--- src/app/components/liste-adherent/liste-adherent.component.ts
+++ src/app/components/liste-adherent/liste-adherent.component.ts
@@ -12,14 +12,20 @@ export class ListeAdherentComponent implements OnInit {
   [x: string]: any;
   public adherents :any;
   public adherentsInitiaux :any;
-  visibleItems = 2;
-  conteur=0;
+  
   query !:any;
   supprimer=false;
   showConfirmationDialog = false;
   userID :any;
   nb_resultats: number | null = null;
   nb_adherents: number | null = null;
+
+  itemsPerPage: number = 2; // Nombre d'utilisateurs à afficher par page.
+  totalPages: number = 1; // Nombre total de pages.
+  currentPage: number = 1; // Page actuelle.
+  pages: number[] = []; // Tableau des numéros de page.
+  displayedUsers: any; 
+
   constructor( private UserService :  UtilisateurService) { }
 
   ngOnInit(): void {
@@ -27,27 +33,55 @@ export class ListeAdherentComponent implements OnInit {
       this.adherents = data;
       this.nb_adherents = this.adherents.length;
       this.adherentsInitiaux=data;
-      this.adherents.slice(this.conteur , this.visibleItems);
+
+      this.totalPages = Math.ceil(this.adherents.length / this.itemsPerPage);
+      this.pages = Array.from({length: this.totalPages}, (_, i) => i + 1);
+      this.displayedUsers = this.getUsersForPage(this.currentPage);
+    
 
     })
   }
+  getUsersForPage(page: number): any[] {
+    // Calcul des utilisateurs à afficher pour la page donnée.
+    const startIndex = (page - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    return this.adherents.slice(startIndex, endIndex);
+  }
+
+  goToPage(page: number): void {
+    // Changement de la page actuelle.
+    this.currentPage = page;
+    this.displayedUsers = this.getUsersForPage(page);
+  }
+
+  nextPage(): void {
+    // Passage à la page suivante.
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+      this.displayedUsers = this.getUsersForPage(this.currentPage);
+    }
+  }
+
+  prevPage(): void {
+    // Passage à la page précédente.
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.displayedUsers = this.getUsersForPage(this.currentPage);
+    }
+  }
+
 
   onInputChange(): void {
     if (this.query === '') {
       this.adherents = this.adherentsInitiaux;// Réinitialise la liste des utilisateurs lorsque le champ de recherche est vide
-      this.adherents.slice(this.conteur , this.visibleItems);
+      this.totalPages = Math.ceil(this.adherents.length / this.itemsPerPage);
+      this.pages = Array.from({length: this.totalPages}, (_, i) => i + 1);
+      this.displayedUsers = this.getUsersForPage(this.currentPage);
       this.nb_resultats=null;
     }
   }
 
-  VoirSuivant() {
-    this.visibleItems += 2;
-    this.conteur +=2;
-  }
-  VoirPrecedent() {
-    this.visibleItems -= 2;
-    this.conteur -=2;
-  }
+  
 
   openConfirmationDialog(id : any) {
     this.userID = id;
@@ -72,7 +106,9 @@ export class ListeAdherentComponent implements OnInit {
       this.UserService.ListeDesUtilisateurs('adhérent').subscribe((data)=>{
         this.adherents = data;
         this.adherentsInitiaux=data;
-        this.adherents.slice(this.conteur , this.visibleItems);
+        this.totalPages = Math.ceil(this.adherents.length / this.itemsPerPage);
+        this.pages = Array.from({length: this.totalPages}, (_, i) => i + 1);
+        this.displayedUsers = this.getUsersForPage(this.currentPage);
 
       })
       this.closeConfirmationDialog();
@@ -86,8 +122,12 @@ export class ListeAdherentComponent implements OnInit {
     this.UserService.RechercherUtilisateur('adhérent',this.query).subscribe((data)=>{
       this.adherents = data;
       this.nb_resultats = this.adherents.length;
+      
+      this.totalPages = Math.ceil(this.adherents.length / this.itemsPerPage);
+      this.pages = Array.from({length: this.totalPages}, (_, i) => i + 1);
+      this.displayedUsers = this.getUsersForPage(this.currentPage);
 
-      this.adherents.slice(this.conteur , this.visibleItems);
+     
 
 
     })
