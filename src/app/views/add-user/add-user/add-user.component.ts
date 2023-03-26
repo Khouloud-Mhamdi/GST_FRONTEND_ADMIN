7374a8c43a96e2271727f1bafe906973a7ac6600
khouloud <khouloudmhamdi7@gmail.com>
2023-03-26 11:13:37 +0100

    controle saisie

--- src/app/views/add-user/add-user/add-user.component.ts
+++ src/app/views/add-user/add-user/add-user.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {  FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
 import { DisciplineService } from 'src/app/services/discipline.service';
+
 
 @Component({
   selector: 'app-add-user',
@@ -9,13 +10,19 @@ import { DisciplineService } from 'src/app/services/discipline.service';
   styleUrls: ['./add-user.component.css']
 })
 export class AddUserComponent implements OnInit {
+
   addUserForm !: FormGroup <any> ;
-  public clubs : any ; 
-  showDisciplines = false ; 
+  public clubs : any ;
+  showDisciplines = false ;
   FormGroup: any;
-  ajout = false ; 
-  erreur = false ; 
-  showConfirmationDialog = false ; 
+  ajout = false ;
+  erreur = false ;
+  invalid=false;
+  showConfirmationDialog = false ;
+  valid=false;
+
+  emailExistence :any;
+
   constructor(private authService : AuthService , private disciplineService : DisciplineService , private formBuilder :FormBuilder) { }
 
   ngOnInit(): void {
@@ -30,38 +37,65 @@ export class AddUserComponent implements OnInit {
      }
      );
      this.getAllclubs();
+
+
+
   }
+
   getAllclubs()
   {
     this.disciplineService.ListerDisciplines().subscribe((data)=>{
-    this.clubs = data ; 
+    this.clubs = data ;
     })
   }
-  
-  addUser(){
 
-    console.log(this.addUserForm.value); 
+  addUser(){
+    if((!this.controleSaisieNom())||(!this.controleSaisiePrénom())||(!this.controleSaisieTelephone())||(!this.controleSaisieEmail()))
+    {this.valid=true;
+      console.log(this.valid);
+      setTimeout(() => {
+        this.valid = false;
+      }, 3000);
+
+  }
+    else{
+      this.authService.ExistEmail(this.addUserForm.value.email).subscribe(
+        (exist:boolean) => {
+          if (exist === true )
+          {this.erreur=true;
+
+            setTimeout(() => {
+              this.erreur = false;
+            }, 4000);}
+          }
+
+      )
+
+    console.log(this.addUserForm.value);
     this.authService.addNewUser(this.addUserForm.value).subscribe(
       (data) => {
-        console.log(data); 
-        this.ajout=true; 
+
+        console.log(data);
+        this.ajout=true;
         setTimeout(() => {
           this.ajout = false;
         }, 3000); // 3000 ms = 3 secondes
       },
       (err) => {
         console.log("here error from BE", err);
-        this.erreur=true; 
+        this.emailExistence=err;
+
+        this.valid=true;
         setTimeout(() => {
-          this.erreur = false;
+          this.valid = false;
         }, 3000); // 3000 ms = 3 secondes
       }
-      
-    ); 
-    this.closeConfirmationDialog(); 
+
+    );}
+    this.closeConfirmationDialog();
   }
-  
-  // event of role selection 
+
+  // event of role selection
  onRoleChange() {
     console.log("hello", this.showDisciplines)
     const selectedRole = this.addUserForm?.get('role')?.value;
@@ -71,15 +105,71 @@ export class AddUserComponent implements OnInit {
       this.showDisciplines = false;
     }
   }
-  
+
+
+  controleSaisieTelephone(): boolean {
+    const telephoneInput = document.getElementById("telephone") as HTMLInputElement;
+
+    if (!/^\d{8}$/.test(this.addUserForm.value.telephone)) {
+      telephoneInput.classList.add("invalid");
+      return false;
+    } else {
+      telephoneInput.classList.remove("invalid");
+      return true;
+    }
+  }
+  controleSaisieNom(): boolean {
+
+    const nomInput = document.getElementById("firstname") as HTMLInputElement;
+    console.log(nomInput);
+    const regex =/^[a-zA-Z]{3,}$/ ;
+    if (!regex.test(this.addUserForm.value.firstName)) {
+      nomInput.classList.add("invalid");
+
+      return false;
+    } else {
+      nomInput.classList.remove("invalid");
+      return true;
+    }
+  }
+  controleSaisiePrénom(): boolean {
+
+    const nomInput = document.getElementById("lastname") as HTMLInputElement;
+    console.log(nomInput);
+    const regex =/^[a-zA-Z]{3,}$/ ;
+    if (!regex.test(this.addUserForm.value.lastName)) {
+      nomInput.classList.add("invalid");
+
+      return false;
+    } else {
+      nomInput.classList.remove("invalid");
+      return true;
+    }
+  }
+  controleSaisieEmail(): boolean {
+
+    const nomInput = document.getElementById("email") as HTMLInputElement;
+    console.log(nomInput);
+    const regex =/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/ ;
+    if (!regex.test(this.addUserForm.value.email)) {
+      nomInput.classList.add("invalid");
+
+      return false;
+    } else {
+      nomInput.classList.remove("invalid");
+      return true;
+    }
+  }
+
+
   openConfirmationDialog(){
     this.showConfirmationDialog = true;
   }
-  
+
   closeConfirmationDialog(){
     this.showConfirmationDialog = false;
   }
-  
- 
+
+
 
 }
